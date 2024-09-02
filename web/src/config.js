4eeaa3b9f9f0784096c9d3c7b8b4ebdf5da13232
Yuan78 <zhaoziyu21@mails.tsinghua.edu.cn>
2024-09-03 00:11:13 +0800

    ASTA Website 20240903

--- web/src/config.js
+++ web/src/config.js
@@ -1,10 +1,10 @@
 global.constants = {
     //server:'https://fcapi.thu-ai.net/',
-    // server:'http://127.0.0.1:8000/',
+    server:'http://127.0.0.1:8000/',
     //server:'http://166.111.73.151:8002/',
     //server:'http://182.92.230.1:81/',
     //server:'http://www.thuasta.cn:81/',
-    server:'https://www.thuasta.cn:8000/',
+    //server:'https://www.thuasta.cn:8000/',
 };
 
 export function getColorFromStatus(status){
